c0a51ffb32993c0deb0d86746dc69775eac16c28
anneletta <144317669+anneletta@users.noreply.github.com>
2024-02-25 22:44:46 +0300

    cosmos

---
+++ Asteroid.cpp
@@ -0,0 +1,27 @@
+Asteroid::Asteroid(char c, char s, bool e, float r, float g) {
+	chemEl = c;
+	spectralClass = s;
+	emitsLight = e;
+	radius = r;
+	gravitation = g;
+}
+char Asteroid::spectralClassType(char chemEl) {
+	if (chemEl == "Carbon") {
+		return "C";
+	}
+	else if (chemEl == "Silicium") {
+		return "S";
+	}
+	else {
+		return "M";
+	}
+}
+void Asteroid::getSpectralClassType() {
+	cout << spectralClassType(chemEl) << endl;
+}
+float Asteroid::getGravitation() {
+	return 6.67 * mass / radius;
+}
+bool Asteroid::hasLight() {
+	return emitsLight;
+}
