284d011f61141c250ffd25022020dfa6b08f0c4e
anneletta <144317669+anneletta@users.noreply.github.com>
2024-02-25 22:53:02 +0300

    cosmos

---
+++ Meteorite.cpp
@@ -0,0 +1,11 @@
+Meteorite::Meteorite(float s, char r) {
+	speed = s;
+	energy = e;
+	regmaForm = r;
+}
+float Meteorite::getEnergy() {
+	return mass * pow(speed, 2) / 2;
+}
+void Meteorite::regmaForm() {
+	cout << regmaForm << endl; 
+}
